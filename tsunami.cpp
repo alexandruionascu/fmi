0e1d8fa22dc5645c8bcc2b7b38a399a263d962d7
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2015-12-14 20:14:32 +0200

    added tsunami

--- tsunami.cpp
+++ tsunami.cpp
@@ -1,112 +1,95 @@
-#include<fstream>
 #include<iostream>
+#include<fstream>
 #include<queue>
+#define MAXN 1002
 #define FIN "tsunami.in"
 #define FOUT "tsunami.out"
-#define MAXN 1004
+#define in f
+#define out g
 using namespace std;
 
 ifstream f(FIN);
 ofstream g(FOUT);
 
-int n, m, h;
-int A[MAXN][MAXN];
-queue< pair<int, int> > t;
-bool viz[MAXN][MAXN];
-int victims;
+int n, m, h, counter;
+int a[MAXN][MAXN];
+queue< pair<int,int> > q;
+bool visited[MAXN][MAXN];
 
-bool valid(int i, int j)
+
+int dirX[] = {-1, 0, 1, 0};
+int dirY[] = {0, -1, 0, 1};
+
+bool check(int i, int j)
 {
-    if(A[i+1][j] > 0 || A[i-1][j] > 0 || A[i][j+1] > 0 || A[i][j-1] > 0)
-        return true;
-    else return false;
-}
+    if(i < 1 || j < 1 || i > n || j > m)
+        return false;
 
+    if(visited[i][j] == true)
+    {
+        return false;
+    }
+    else
+    {
+        if(a[i][j] > 0)
+        {
+            if(a[i][j] < h)
+                return true;
+            else return false;
+        }
+
+        return true;
+    }
+}
 int read()
 {
-    f >> n;
-    f >> m;
-    f >> h;
+    in >> n;
+    in >> m;
+    in >> h;
 
-
-    for(int i=1; i<=n; i++)
+    for(int i = 1; i <= n; i++)
     {
-        for(int j=1; j<=m; j++)
+        for(int j = 1; j <= m; j++)
         {
-            f >> A[i][j];
+            f >> a[i][j];
+
+            if(a[i][j] == 0)
+            {
+                q.push(make_pair(i, j));
+            }
         }
     }
 
-
     return 0;
 }
-
 
 int solve()
 {
-    for(int i=1; i<=n; i++)
+    while(q.empty() == false)
     {
-        for(int j=1; j<=m; j++)
-        {
-            if(A[i][j] == 0 && (i == 1 || j == 1 || i == n || j == m))
-            {
-                t.push(make_pair(i, j));
-            }
+        pair<int, int> p = q.front();
+        q.pop();
+        int x = p.first;
+        int y = p.second;
+        visited[x][y] = true;
 
+        if(a[x][y] != 0)
+        {
+            counter++;
+        }
+
+
+        for(int i = 0; i < 4; i++)
+        {
+            if(check(x + dirX[i], y + dirY[i]) == true)
+            {
+                q.push(make_pair(x + dirX[i], y + dirY[i]));
+                visited[x + dirX[i]][y + dirY[i]] = true;
+            }
         }
     }
 
-    for(int i=0; i<=n+1; i++)
-        A[i][0] = MAXN, A[i][m+1] = MAXN;
-
-    for(int i=0; i<=m+1; i++)
-        A[0][i] = MAXN, A[n+1][i] = MAXN;
-
-    while(!t.empty())
-    {
-        int i = t.front().first;
-        int j = t.front().second;
-
-        t.pop();
-
-        if(viz[i][j] == false)
-        {
-            if(A[i][j] > 0)
-            {
-                victims++;
-            }
-
-
-            if(A[i+1][j] < h)
-            {
-                t.push(make_pair(i+1, j));
-            }
-            else if(A[i-1][j] < h)
-            {
-                t.push(make_pair(i-1, j));
-            }
-            else if(A[i][j+1] < h)
-            {
-                t.push(make_pair(i, j+1));
-            }
-            else if(A[i][j-1] < h)
-            {
-                t.push(make_pair(i, j-1));
-            }
-
-        }
-
-        viz[i][j] = true;
-
-
-
-    }
-    return 0;
-}
-
-int write()
-{
-    g << victims;
+    out << counter;
     return 0;
 }
 
@@ -114,7 +97,5 @@ int main()
 {
     read();
     solve();
-    write();
-
     return 0;
 }
