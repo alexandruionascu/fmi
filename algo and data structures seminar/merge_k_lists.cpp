4853f4413f9730c600e4466135075cf2658b269f
Alexandru <alexandruionascu.bd@gmail.com>
2016-01-02 18:52:11 +0200

    added merge k lists on leetcode

--- algo and data structures seminar/merge_k_lists.cpp
+++ algo and data structures seminar/merge_k_lists.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
+#define in f
+#define out cout
 using namespace std;
 
+ifstream f("date.in");
 
 typedef struct ListNode {
      int val;
@@ -9,42 +12,85 @@ typedef struct ListNode {
 };
 
 ListNode* mergeKLists(vector<ListNode*>& lists) {
-        multiset<int> s;
-        ListNode* aux = NULL;
-        ListNode* sol = NULL;
+        multiset< pair<int, int> >  s;
+        ListNode* beginning = NULL;
+        ListNode* node = NULL;
 
+        for(int i = 0; i < lists.size(); i++) {
+            if(lists[i] != NULL) {
+                s.insert(make_pair(lists[i]->val, i));
+                lists[i] = lists[i]->next;
+            }
+        }
 
+        while(!s.empty()) {
+            int index = s.begin()->second;
 
-        while(lists.size() > 0) {
-            for(int i = 0; i < lists.size(); i++) {
-                if(lists[i] != NULL) {
-                    s.insert(lists[i]->val);
-                    lists[i] = lists[i]->next;
-                }
-
-                if(lists[i] == NULL) {
-                    lists.erase(lists.begin() + i);
-                    i--;
-                }
+            if(node == NULL) {
+                node = new ListNode(s.begin()->first);
+            } else {
+                node->next = new ListNode(s.begin()->first);
+                node = node->next;
             }
 
-            for(int i = 0; i < s.size(); i++) {
-                aux = new ListNode(*s.begin());
-                if(sol == NULL)
-                    sol = aux;
-                aux = aux->next;
-                s.erase(s.begin());
+            if(beginning == NULL)
+                beginning = node;
+
+            s.erase(s.begin());
+
+            while(lists[index] != NULL) {
+                s.insert(make_pair(lists[index]->val, index));
+                lists[index] = lists[index]->next;
             }
+        }
+
+        return beginning;
+}
+
+void printList(ListNode* node) {
+    while(node != NULL) {
+        out << node->val << " ";
+        node = node->next;
+    }
+
+    out << "\n";
+}
+
+int main() {
+
+    int k, n, x;
+    vector<ListNode*> v;
+    in >> k;
+
+    for(int i = 1; i <= k; i++) {
+        in >> n;
+
+        ListNode* node = NULL;
+        ListNode* beginning = NULL;
+        for(int j = 1; j <= n; j++) {
+            in >> x;
+            if(node == NULL)
+                node = new ListNode(x);
+            else {
+                node->next = new ListNode(x);
+                node = node->next;
+            }
+
+            if(beginning == NULL)
+                beginning = node;
 
         }
 
-        return sol;
+        v.push_back(beginning);
     }
 
-int main() {
-    vector<ListNode*> v;
-    v.push_back(new ListNode(1));
-    ListNode* node = mergeKLists(v);
 
-    cout << node->val;
+    for(int i = 0; i < v.size(); i++) {
+        printList(v[i]);
+    }
+
+    ListNode* ans = mergeKLists(v);
+    printList(ans);
+
+    return 0;
 }
