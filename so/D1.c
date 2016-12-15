f6d0bd2378377a65a9cdfa7dfe93c50eb1451750
Abel <alexandruionascu@gmail.com>
2016-12-15 10:52:31 +0200

    added new SO exercise

--- so/D1.c
+++ so/D1.c
@@ -2,16 +2,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<errno.h>
 
-int isValidDirectory(char const* dirName) {
+void printFile(char const* fileName, int depth) {
+  for(int i = 1; i <= depth; i++) {
+    printf("----");
+  }
+
+  printf("%s\n", fileName);
+}
+
+int isValid(char const* dirName) {
   if(strcmp(dirName, ".") == 0 )
     return 0;
 
   if(strcmp(dirName, "..") == 0)
-    return 0;
-
-  DIR* directory = opendir(dirName);
-  if(!directory)
     return 0;
 
   return 1;
@@ -19,38 +26,56 @@ int isValidDirectory(char const* dirName) {
 
 void showErrorAndExit() {
   printf("Error reading the directory...\n");
+  if(errno == 24) {
+    printf("Too many files \n");
+  }
   exit(EXIT_FAILURE);
 }
 
-void listDirectory(char const* dirName) {
-  DIR* directory = opendir(dirName);
+void listDirectory(char const* dirName, int depth) {
+  chdir(dirName);
+  DIR* directory = opendir(".");
+
   if(!directory) {
     showErrorAndExit();
   }
 
+  printFile(dirName, depth);
+
   struct dirent* entry = readdir(directory);
 
   while(entry != NULL) {
-    if(entry->d_type == DT_DIR) {
-      if(isValidDirectory(entry->d_name)) {
-        printf("DIR %s \n", entry->d_name);
-        listDirectory(entry->d_name);
+    struct stat buffer;
+    int status;
+    status = lstat(entry->d_name, &buffer);
+
+    if(isValid(entry->d_name)) {
+      if(status == 0 && S_ISDIR(buffer.st_mode)) {
+        listDirectory(entry->d_name, depth + 1);
+      } else {
+        printFile(entry->d_name, depth);
       }
-    } else {
-      printf("FILE %s \n", entry->d_name);
     }
+
 
     entry = readdir(directory);
   }
 
+  chdir("..");
 }
+
+void treeF(char const* dirName) {
+  listDirectory(dirName, 0);
+}
+
+
 
 int main(int argc, char** argv) {
   if(argc < 2) {
     showErrorAndExit();
   }
 
-  listDirectory(argv[1]);
+  treeF(argv[1]);
 
   return 0;
 }
