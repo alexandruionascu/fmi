43a45c3c29beef5a75881b9f1c36b5fd454137f5
Abel <alexandruionascu.bd@gmail.com>
2017-01-07 15:06:38 +0200

    more web lab homework

---
+++ tehnici_web/lab6/Task_A/hover.js
@@ -0,0 +1,26 @@
+// Function to add event listener to table
+var firstParagraph = document.getElementById("first_paragraph");
+var secondParagraph = document.getElementById("second_paragraph");
+firstParagraph.addEventListener("mouseover", function() {
+  secondParagraph.setAttribute("style", "background-color: orange");
+});
+secondParagraph.addEventListener("mouseover", function() {
+  firstParagraph.setAttribute("style", "background-color:yellow");
+});
+
+var searchButton = document.getElementById("search-btn");
+searchButton.addEventListener("click", function() {
+  var query = window.prompt("Please type the search query");
+  var words = query.split(" ");
+  var word = words[0];
+  var paragraph = parseInt(words[1]);
+
+  var text;
+  if(paragraph == 1) {
+    text = firstParagraph.innerHTML;
+  } else {
+    text = secondParagraph.innerHTML;
+  }
+
+  alert(text.split(word).length - 1);
+});
